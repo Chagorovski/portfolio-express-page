dcb55356966528f020d2a102b20eb3a74a14b36a
Chagorovski <52652042+Chagorovski@users.noreply.github.com>
2019-11-10 19:23:26 +0100

    bugs fixed

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { projects } = require('../data');
+const { projects } = require('../data/data.json');
 
-router.get('/', (req, res) => {
-    res.render('./', { projects });
+router.get('/',(req, res) => {
+    res.render('./', { projects: projects });
 });
 
 module.exports = router;
