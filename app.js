24b2abb962c153da21ee0549db85f966bcdaa8f8
Chagorovski <52652042+Chagorovski@users.noreply.github.com>
2019-11-10 12:45:33 +0100

    added middleware for errors

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
-    homeRoute = require('./routes');
-    about = require('./routes/about');
-    projects = require('./routes/projects');  
+      homeRoute = require('./routes');
+      about = require('./routes/about');
+      projects = require('./routes/projects');  
 
 const app = express();
 
@@ -15,3 +15,16 @@ app.use('/static', express.static('public'));
 app.use(homeRoute);
 app.use('/about', about);
 app.use('/project', projects);
+
+// Middleware for Errors
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+  
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status);
+    res.render('error');
+});
