1116362d77b52ed20e0dc2bb0f23261ca9eeeabd
Chagorovski <52652042+Chagorovski@users.noreply.github.com>
2019-11-11 12:31:19 +0100

    bugs fixed

--- routes/projects.js
+++ routes/projects.js
@@ -3,12 +3,7 @@ const router = express.Router();
 const data = require('../data/data.json');
 const projects = data.projects;
 
-//redirects to the first project page if someone trys to go to the projects url without an id entered
-router.get('/', (req, res) => {
-    res.redirect(`/projects/0`);
-});
 
-//dynamically renders a project page based on the id of the project
 router.get('/:id', (req, res) => {
     const id = parseFloat(req.params.id);
     const project = projects[id]
